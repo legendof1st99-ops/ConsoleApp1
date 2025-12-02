518a6192199e479ccacac897f7635e4039298144
oqwhitecat <legendof1st99@gmail.com>
2025-12-02 14:25:33 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the console
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("my name is, Pannatorn Srita");
+            Console.WriteLine("student id: 683450056-9");
+            Console.WriteLine("major: Computer and infomation science");
+            Console.WriteLine("faculty: IS");
+            Console.WriteLine("khon kaen university");
+
+        }
+    }
+}
